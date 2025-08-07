fd067f5de7aaaceee6947f5807fd6d953dd8e286
mrpoth <adam.pothitos@gmail.com>
2025-08-07 13:40:46 +0100

    Swapped examples-4-csv location for ease

--- app/Http/Controllers/HomeownerController.php
+++ app/Http/Controllers/HomeownerController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Services\PersonParserService;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class HomeownerController extends Controller
 {
     public function __invoke(PersonParserService $personParserService): Collection
     {
-        return $personParserService->getHomeOwnerNames(Storage::get('examples-4-.csv'));
+        return $personParserService
+            ->getHomeOwnerNames(
+                File::get(resource_path('data/examples-4-.csv'))
+            );
     }
 }
